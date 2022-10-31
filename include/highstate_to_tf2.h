bae50998cbd4e691219def0fc3ca29dc0a2f19e4
Jonas Grube <jonas.grube@fh-bielefeld.de>
2022-10-31 10:21:19 +0100

    implemented highstate to tf2 converter

---
+++ include/highstate_to_tf2.h
@@ -0,0 +1,19 @@
+#ifndef HIGHSTATE_TO_TF2_H
+#define HIGHSTATE_TO_TF2_H
+
+#include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <unitree_legged_msgs/HighState.h>
+
+ros::Subscriber sub;
+tf2_ros::TransformBroadcaster tfb;
+geometry_msgs::TransformStamped go1_transform;
+geometry_msgs::TransformStamped foot_transform;
+
+const char*[] feet = {"FR", "FL", "RR", "RL"};
+
+void state_callback(const unitree_legged_msgs::HighState::ConstPtr &state);
+
+#endif  // HIGHSTATE_TO_TF2_H
---
+++ src/highstate_to_tf2.cpp
@@ -0,0 +1,49 @@
+#include "../include/highstate_to_tf2.h"
+
+
+void state_callback(const unitree_legged_msgs::HighState::ConstPtr &state){
+    go1_transform.header.frame_id = "world";
+    go1_transform.child_frame_id = "go1";
+
+    go1_transform.transform.translation.x = state.position[0];
+    go1_transform.transform.translation.y = state.position[1];
+    go1_transform.transform.translation.z = state.position[2];
+
+    tf2::Quaternion q;
+    q.setRPY(state.euler[0],state.euler[1],state.euler[2]);
+    go1_transform.transform.rotation.x = q.x();
+    go1_transform.transform.rotation.y = q.y();
+    go1_transform.transform.rotation.z = q.z();
+    go1_transform.transform.rotation.w = q.w();
+    
+    go1_transform.header.stamp = ros::Time::now();
+    tfb.sendTransform(go1_transform);
+
+    for (int i=0; i<4;++i){
+        foot_transform.header.frame_id = "go1";
+        foot_transform.child_frame_id = feet[i];
+
+        foot_transform.transform.translation.x = state.footPosition2Body[i].x;
+        foot_transform.transform.translation.y = state.footPosition2Body[i].y;
+        foot_transform.transform.translation.z = state.footPosition2Body[i].z;
+
+        tf2::Quaternion q;
+        q.setRPY(0, 0, 0);
+        foot_transform.transform.rotation.x = q.x();
+        foot_transform.transform.rotation.y = q.y();
+        foot_transform.transform.rotation.z = q.z();
+        foot_transform.transform.rotation.w = q.w();
+
+        foot_transform.header.stamp = ros::Time::now();
+        tfb.sendTransform(foot_transform);
+    }
+
+}
+
+
+int main(int argc, char* argv){
+    ros::init(argc, argv, "highstate_to_tf2");
+    ros::NodeHandle nh;
+
+    sub = nh.subscribe("/high_state",1, state_callback);
+}
