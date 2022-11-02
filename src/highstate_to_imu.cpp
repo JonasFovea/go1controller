95643bac096852540250fc0c0e8ffe94180a60e1
Jonas Grube <jonas.grube@fh-bielefeld.de>
2022-11-02 09:32:17 +0100

    copy artefact removed

--- src/highstate_to_imu.cpp
+++ src/highstate_to_imu.cpp
@@ -21,8 +21,7 @@ int main(int argc, char** argv){
     printf("\n\n\n[i] started highstate_to_imu\n");
     ros::init(argc, argv, "high2imu");
     ros::NodeHandle nh;
-
-    tfb = new tf2_ros::TransformBroadcaster();
+    
     pub = nh.advertise<geometry_msgs::Imu>("/imu", 1000);
     sub = nh.subscribe("/high_state",1, state_callback);
     ros::spin();
