5be52b000a7be88390afef4b568a83c1d978144d
Jonas Grube <jonas.grube@fh-bielefeld.de>
2022-11-02 14:35:36 +0100

    renaming fix

--- src/highstate_to_tf2.cpp
+++ src/highstate_to_tf2.cpp
@@ -38,47 +38,47 @@ void state_callback(const unitree_legged_msgs::HighState& state){
         tfb->sendTransform(foot_transform);
     }
 
-    transformStamped.header.frame_id = "go1";
-    transformStamped.child_frame_id = "go1_front";
-    transformStamped.transform.translation.x = 0.30;
-    transformStamped.transform.translation.y = 0.0;
-    transformStamped.transform.translation.z = 0.0;
+    sensor_transform.header.frame_id = "go1";
+    sensor_transform.child_frame_id = "go1_front";
+    sensor_transform.transform.translation.x = 0.30;
+    sensor_transform.transform.translation.y = 0.0;
+    sensor_transform.transform.translation.z = 0.0;
     tf2::Quaternion q1;
     q1.setRPY(0, 0, 0);
-    transformStamped.transform.rotation.x = q1.x();
-    transformStamped.transform.rotation.y = q1.y();
-    transformStamped.transform.rotation.z = q1.z();
-    transformStamped.transform.rotation.w = q1.w();
-    transformStamped.header.stamp = ros::Time::now();
-    tfb->sendTransform(transformStamped);
+    sensor_transform.transform.rotation.x = q1.x();
+    sensor_transform.transform.rotation.y = q1.y();
+    sensor_transform.transform.rotation.z = q1.z();
+    sensor_transform.transform.rotation.w = q1.w();
+    sensor_transform.header.stamp = ros::Time::now();
+    tfb->sendTransform(sensor_transform);
 
     tf2::Quaternion q2;
-    transformStamped.header.frame_id = "go1";
-    transformStamped.child_frame_id = "go1_left";
-    transformStamped.transform.translation.x = -0.06;
-    transformStamped.transform.translation.y = 0.10;
-    transformStamped.transform.translation.z = 0.0;
+    sensor_transform.header.frame_id = "go1";
+    sensor_transform.child_frame_id = "go1_left";
+    sensor_transform.transform.translation.x = -0.06;
+    sensor_transform.transform.translation.y = 0.10;
+    sensor_transform.transform.translation.z = 0.0;
     q2.setRPY(0, 0, 1.5708f);
-    transformStamped.transform.rotation.x = q2.x();
-    transformStamped.transform.rotation.y = q2.y();
-    transformStamped.transform.rotation.z = q2.z();
-    transformStamped.transform.rotation.w = q2.w();
-    transformStamped.header.stamp = ros::Time::now();
-    tfb->sendTransform(transformStamped);
+    sensor_transform.transform.rotation.x = q2.x();
+    sensor_transform.transform.rotation.y = q2.y();
+    sensor_transform.transform.rotation.z = q2.z();
+    sensor_transform.transform.rotation.w = q2.w();
+    sensor_transform.header.stamp = ros::Time::now();
+    tfb->sendTransform(sensor_transform);
 
     tf2::Quaternion q3;
-    transformStamped.header.frame_id = "go1";
-    transformStamped.child_frame_id = "go1_right";
-    transformStamped.transform.translation.x = -0.06;
-    transformStamped.transform.translation.y = -0.10;
-    transformStamped.transform.translation.z = 0.0;
+    sensor_transform.header.frame_id = "go1";
+    sensor_transform.child_frame_id = "go1_right";
+    sensor_transform.transform.translation.x = -0.06;
+    sensor_transform.transform.translation.y = -0.10;
+    sensor_transform.transform.translation.z = 0.0;
     q3.setRPY(0, 0, -1.5708f);
-    transformStamped.transform.rotation.x = q3.x();
-    transformStamped.transform.rotation.y = q3.y();
-    transformStamped.transform.rotation.z = q3.z();
-    transformStamped.transform.rotation.w = q3.w();
-    transformStamped.header.stamp = ros::Time::now();
-    tfb->sendTransform(transformStamped);
+    sensor_transform.transform.rotation.x = q3.x();
+    sensor_transform.transform.rotation.y = q3.y();
+    sensor_transform.transform.rotation.z = q3.z();
+    sensor_transform.transform.rotation.w = q3.w();
+    sensor_transform.header.stamp = ros::Time::now();
+    tfb->sendTransform(sensor_transform);
 
 }
 
