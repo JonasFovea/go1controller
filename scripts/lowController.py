1dfbe81e5fbde8b5643f86a993600c4e36fa20a9
Jonas Grube <jonas.grube@fh-bielefeld.de>
2022-11-17 08:42:14 +0100

    added script to collect angle data

---
+++ scripts/lowController.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import rospy
+from go1_legged_msgs.msg import LowCmd
+from go1_legged_msgs.msg import LowState
+from go1_legged_msgs.msg import MotorCmd
+from go1_legged_msgs.msg import MotorState
+
+HIGHLEVEL = 0xEE
+LOWLEVEL = 0xFF
+TRIGERLEVEL = 0xF0
+
+PosStopF = 2.146E+9
+VelStopF = 16000.0
+
+PMSM = 0x0A
+BRAKE = 0x00
+
+counter = 0
+samples = []
+
+
+def init_low_command() -> LowCmd:
+    cmd_msg = LowCmd()
+    cmd_msg.head = [0xFE, 0xEF]
+    cmd_msg.levelFlag = LOWLEVEL
+
+    for mcmd in cmd_msg.motorCmd:
+        mcmd.mode = PMSM
+        mcmd.q = 0
+        mcmd.dq = 0
+        mcmd.tau = 0
+        mcmd.Kp = 50.0
+        mcmd.Kd = 5.0
+
+    return cmd_msg
+
+
+def set_motor_pos_step() -> MotorCmd:
+    mcmd = MotorCmd()
+    mcmd.mode = PMSM
+    mcmd.q = PosStopF
+    mcmd.dq = 0
+    mcmd.tau = 0
+    mcmd.Kp = 50.0
+    mcmd.Kd = 5.0
+    return mcmd
+
+
+def low_state_tracker(msg: LowState):
+    global counter  # yes, I didn't want to do this, but it's the fastest solution
+    # global samples  # yes, I didn't want to do this, but it's the fastest solution
+
+    samples.append((counter, msg.motorState[2].q, msg.motorState[2].q_raw))
+
+
+
+def test():
+    pub = rospy.Publisher("go1_controller/low_cmd", LowCmd, queue_size=1)
+    rospy.Subscriber("go1_controller/low_state", LowState, low_state_tracker, queue_size=10)
+    rospy.init_node("low_controller", anonymous=True)
+
+    global counter  # yes, I didn't want to do this, but it's the fastest solution
+
+    rate = rospy.Rate(10)
+
+    cmd_msg = init_low_command()
+
+    if not rospy.is_shutdown():
+        rospy.loginfo(cmd_msg)
+        pub.publish(cmd_msg)
+        rate.sleep()
+
+        counter += 1
+        cmd_msg.motorCmd[2] = set_motor_pos_step()
+        rospy.loginfo(cmd_msg)
+        pub.publish(cmd_msg)
+        rate.sleep()
+
+    while not rospy.is_shutdown():
+        if counter > 10000:
+            break
+
+        counter += 1
+        rate.sleep()
+
+    print("Collected 10000 samples:")
+    for s in samples:
+        print(f"\tt step: {s[0]:05d}\tq: {s[1]}\t q_raw: {s[2]}")
+
+
+
+
+if __name__ == "__main__":
+    try:
+        test()
+    except rospy.ROSInterruptException as e:
+        print(f"ROS Error: {e}\n\nExiting...")
